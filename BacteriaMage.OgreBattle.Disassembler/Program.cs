b438589952a2211c5ddd20e769ece9224f3a384c
BacteriaMage <github@bacteriamage.xsmail.com>
2025-09-06 23:09:28 -0500

    implement basic program startup

--- BacteriaMage.OgreBattle.Disassembler/Program.cs
+++ BacteriaMage.OgreBattle.Disassembler/Program.cs
@@ -2,9 +2,35 @@
 
 namespace BacteriaMage.OgreBattle.Disassembler;
 
-public static class Program
+public class Program
 {
-    public static void Main(string[] args)
+    private void ParseArgs(string[] args)
     {
+        Messenger.ShowVerbose = !args.Contains("-q");
+    }
+
+    private void Run()
+    {
+    }
+
+    private void TryMain(string[] args)
+    {
+        ParseArgs(args);
+        Run();
+    }
+    
+    public static int Main(string[] args)
+    {
+        try
+        {
+            new Program().TryMain(args);
+            return 0;
+        }
+        catch (Exception e)
+        {
+            Messenger.Error(e.Message);
+            Messenger.Verbose(e.StackTrace);
+            return 1;
+        }
     }
 }
