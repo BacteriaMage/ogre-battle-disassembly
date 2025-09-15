257fd9e6b60afae082beeba3e414d41f1ddc515a
BacteriaMage <github@bacteriamage.xsmail.com>
2025-09-14 22:39:24 -0500

    create labels infrastructure

---
+++ BacteriaMage.OgreBattle.Disassembler/Disassembly/Label.cs
@@ -0,0 +1,73 @@
+﻿// github.com/BacteriaMage
+
+using BacteriaMage.OgreBattle.Disassembler.Types;
+
+namespace BacteriaMage.OgreBattle.Disassembler.Disassembly;
+
+/// <summary>
+/// Represents a labeled location in memory or ROM, with associated metadata.
+/// </summary>
+public class Label(Address address)
+{
+    private bool _forCode;
+    private bool _forData;
+
+    /// <summary>
+    /// The address of the label is the disassembly listing.
+    /// </summary>
+    public readonly Address Address = address;
+
+    /// <summary>
+    /// The name of the label as it appears in the listing.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Indicates whether the label is a target for branching instructions in the disassembly.
+    /// </summary>
+    public bool BranchTarget { get; set; }
+
+    /// <summary>
+    /// Indicates whether the label serves as a destination for jump instructions in the disassembled code.
+    /// </summary>
+    public bool JumpTarget { get; set; }
+    
+    /// <summary>
+    /// Indicates whether the label serves as a destination for call instructions in the disassembled code.
+    /// </summary>
+    public bool CallTarget { get; set; }
+
+    /// <summary>
+    /// Indicates whether this label is associated with executable code.
+    /// </summary>
+    public bool ForCode
+    {
+        get => _forCode;
+        set
+        {
+            if (_forData && value)
+            {
+                throw new InvalidOperationException("Cannot set label to be for both code and data.");
+            }
+            
+            _forCode = value;
+        }
+    }
+
+    /// <summary>
+    /// Indicates whether the label represents data in memory or ROM.
+    /// </summary>
+    public bool ForData
+    {
+        get => _forData;
+        set
+        {
+            if (_forCode && value)
+            {
+                throw new InvalidOperationException("Cannot set label to be for both code and data.");
+            }
+            
+            _forData = value;
+        }
+    }
+}
---
+++ BacteriaMage.OgreBattle.Disassembler/Disassembly/Labels.cs
@@ -0,0 +1,152 @@
+﻿// github.com/BacteriaMage
+
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using BacteriaMage.OgreBattle.Disassembler.Types;
+
+namespace BacteriaMage.OgreBattle.Disassembler.Disassembly;
+
+/// <summary>
+/// Collection of labels uniquely associated with addresses.
+/// </summary>
+public class Labels : IReadOnlyDictionary<Address, Label>
+{
+    private readonly Dictionary<int, Label> _labels = new();
+
+    /// <summary>
+    /// Retrieves a new or the existing label associated with the specified address.
+    /// </summary>
+    /// <param name="address">The address for the label.</param>
+    /// <returns>The <see cref="Label"/> associated with the specified address.</returns>
+    public Label LabelFor(Address address)
+    {
+        if (!_labels.TryGetValue(address.Long, out var label))
+        {
+            label = new Label(address);
+            _labels.Add(address.Long, label);
+        }
+
+        return label;
+    }
+
+    /// <summary>
+    /// Retrieves a new or the existing label for the specified address
+    /// and marks it as a branch target.
+    /// </summary>
+    /// <param name="address">The address for the label.</param>
+    /// <returns>The <see cref="Label"/> associated with the specified address.</returns>
+    public Label LabelForBranch(Address address)
+    {
+        Label label = LabelFor(address);
+        label.BranchTarget = true;
+        return label;
+    }
+
+    /// <summary>
+    /// Retrieves a new or the existing label for the specified address
+    /// and marks it as a jump target.
+    /// </summary>
+    /// <param name="address">The address for the label.</param>
+    /// <returns>The <see cref="Label"/> associated with the specified address.</returns>
+    public Label LabelForJump(Address address)
+    {
+        Label label = LabelFor(address);
+        label.JumpTarget = true;
+        return label;   
+    }
+
+    /// <summary>
+    /// Retrieves a new or the existing label for the specified address
+    /// and marks it as a call target.
+    /// </summary>
+    /// <param name="address">The address for the label.</param>
+    /// <returns>The <see cref="Label"/> associated with the specified address.</returns>
+    public Label LabelForCall(Address address)
+    {
+        Label label = LabelFor(address);
+        label.CallTarget = true;
+        return label;   
+    }
+    
+    #region Dictionary Interface
+    
+    /// <summary>
+    /// The number of labels in the collection.
+    /// </summary>
+    public int Count
+    {
+        get => _labels.Count;
+    }
+    
+    /// <summary>
+    /// Gets the label associated with the specified address.
+    /// </summary>
+    public Label this[Address key]
+    {
+        get => _labels[key.Long];
+    }
+
+    /// <summary>
+    /// Determines whether the collection contains a label associated with the specified address.
+    /// </summary>
+    /// <param name="key">The address to locate in the collection.</param>
+    /// <returns>True if a label associated with the specified address exists in the collection; otherwise, false.</returns>
+    public bool ContainsKey(Address key)
+    {
+        return _labels.ContainsKey(key.Long);
+    }
+
+    /// <summary>
+    /// Gets the label associated with the specified address.
+    /// </summary>
+    /// <param name="key">The address to locate in the collection.</param>
+    /// <param name="value">When this method returns, contains the label associated with the specified address, if the address is found; otherwise, the default value for the type of the value parameter. This parameter is passed uninitialized.</param>
+    /// <returns>True if a label associated with the specified address exists in the collection; otherwise, false.</returns>
+    public bool TryGetValue(Address key, [MaybeNullWhen(false)] out Label value)
+    {
+        return _labels.TryGetValue(key.Long, out value);
+    }
+
+    /// <summary>
+    /// Gets an enumeration of all addresses for which labels exist in the dictionary.
+    /// </summary>
+    public IEnumerable<Address> Keys
+    {
+        get
+        {
+            foreach (var (key, _) in _labels)
+            {
+                yield return new Address(key);
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Gets an enumeration of all labels in the dictionary.
+    /// </summary>   
+    public IEnumerable<Label> Values
+    {
+        get => _labels.Values;
+    }
+    
+    /// <summary>
+    /// Gets an enumeration of all labels in the dictionary.
+    /// </summary>
+    public IEnumerator<KeyValuePair<Address, Label>> GetEnumerator()
+    {
+        foreach (var (key, value) in _labels)
+        {
+            yield return new KeyValuePair<Address, Label>(new Address(key), value);
+        }
+    }
+
+    /// <summary>
+    /// Gets an enumeration of all labels in the dictionary.   
+    /// </summary>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    
+    #endregion
+}
